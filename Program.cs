85bd66537ea8ec346ad28904d8b6cfed25b0fe51
Claire <97535035+CLg003@users.noreply.github.com>
2022-07-19 15:23:19 +0100

    Final commit.

--- Program.cs
+++ Program.cs
@@ -9,17 +9,27 @@ namespace CaesarCipher
             char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         
             Console.WriteLine("Please enter your secret message: ");
-            String userInput = Console.ReadLine();
+            String userInput = Console.ReadLine().ToLower();
             char[] secretMessage = userInput.ToCharArray();
-            char[] encryptedMessage = char[secretMessage.Length];
+            char[] encryptedMessage = new char[secretMessage.Length];
 
+            // Encrypting message characters
             for (int i = 0; i < secretMessage.Length; i ++) {
                 char character = secretMessage[i];
                 int characterIndex = Array.IndexOf(alphabet, character);
-                int encryptedCharacterIndex = characterIndex + 3;
+                if (characterIndex == -1) {
+                    encryptedMessage[i] = character;
+                } else {
+                    int encryptedCharacterIndex = (characterIndex + 3) % 26;
                 char encryptedCharacter = alphabet[encryptedCharacterIndex];
                 encryptedMessage[i] = encryptedCharacter;
+                }
+                
             }
+
+            String encryptedMessageString = String.Join("", encryptedMessage);
+            Console.WriteLine($"Here is your encrypted message: {encryptedMessageString}");
+
         }
     }
 }
