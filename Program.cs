eb46f1bf9ac93acb5befdaf0c00189bb1460a08a
Claire <97535035+CLg003@users.noreply.github.com>
2022-07-19 15:10:41 +0100

    Added encryption.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,18 @@ namespace CaesarCipher
         {
             char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         
+            Console.WriteLine("Please enter your secret message: ");
+            String userInput = Console.ReadLine();
+            char[] secretMessage = userInput.ToCharArray();
+            char[] encryptedMessage = char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i ++) {
+                char character = secretMessage[i];
+                int characterIndex = Array.IndexOf(alphabet, character);
+                int encryptedCharacterIndex = characterIndex + 3;
+                char encryptedCharacter = alphabet[encryptedCharacterIndex];
+                encryptedMessage[i] = encryptedCharacter;
+            }
         }
     }
 }
